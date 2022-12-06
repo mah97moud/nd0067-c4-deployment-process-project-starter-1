dfc9f3bacba0bf5d085ab14ee89562afd5bf73f3
Mahmoud Atef <mahmoudatif54@yahoo.com>
2022-12-06 20:11:56 +0200

    test

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
-aws s3 cp --recursive --acl public-read ./www s3://mybuckettest123456test/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://mybuckettest123456test/
+aws s3 cp --recursive --acl public-read ./www s3://my-bucket-test1234-mahmoud/
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://my-bucket-test1234-mahmoud/

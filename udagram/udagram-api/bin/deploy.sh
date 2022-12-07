e5c42f52ff3027740e99697a19f8c88c8bbab521
Mahmoud Atef <mahmoudatif54@yahoo.com>
2022-12-08 00:45:30 +0200

    edit deploy file

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,6 +1,16 @@
 eb list
-eb use Testdev-env
-# chmod +x ../../udagram/set_env.sh
-# ../../udagram/set_env.sh
-# eb printenv AWS_BUCKET
+eb use Mahmoudapi-env-1
+eb setenv AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+eb setenv AWS_BUCKET=$AWS_BUCKET
+eb setenv AWS_REGION=$AWS_DEFAULT_REGION
+eb setenv AWS_PROFILE=$AWS_PROFILE
+eb setenv AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+eb setenv JWT_SECRET=$JWT_SECRET
+eb setenv PORT=$PORT
+eb setenv POSTGRES_DB=$POSTGRES_DB
+eb setenv POSTGRES_HOST=$POSTGRES_HOST
+eb setenv POSTGRES_PASSWORD=$POSTGRES_PASSWORD
+eb setenv POSTGRES_PORT=$POSTGRES_PORT
+eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME
+eb setenv URL=$URL
 eb deploy Mahmoudapi-env-1
